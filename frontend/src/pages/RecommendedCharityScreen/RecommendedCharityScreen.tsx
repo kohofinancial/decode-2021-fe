a7b994a7e045324ae0f7d7661bb4649d98cecd2e
James Lee <jklee4365@gmail.com>
2021-03-05 01:14:12 -0500

    Implemented recommended charity screen

---
+++ frontend/src/pages/RecommendedCharityScreen/RecommendedCharityScreen.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { IonList, IonCard, IonGrid, IonRow,IonCol, IonCardContent, IonText } from '@ionic/react';
+import CharityItem from './Components/CharityItem';
+import "./RecommendedCharityScreen.css";
+
+import RecommendedSearchbar from './Components/RecommendedSearchbar';
+
+const RecommendedCharityScreen: React.FC = () => {
+    const [text, setText] = useState<string>("");
+    const [charities, setCharities] = useState<any>();
+
+    useEffect(() => {
+        retrieveAllCharities(text);
+    }, [])
+
+    const retrieveAllCharities = (category: string) => {
+        fetch("https://decode-be-2021.herokuapp.com/charities?category=" + category,{headers: {
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }}).then(res => {
+            return (res.ok) ?  res.json() : new Error("Mistake!")
+        }).then(data => {
+            setCharities(data.results);
+        })
+    }
+
+    const handleKeyDown = (e: any) => {
+        if(e.key === "Enter"){
+            retrieveAllCharities(text);
+        }
+    }
+
+    return (
+        <div className = "recommendCharityScreen">
+            <div className = "header">
+                <div className = "title">Donations</div>
+                <div className = "description">
+                    With everything that’s happening right now, it’s a great time to give back to the community.
+                </div>
+                <IonCard>
+                    <IonCardContent>
+                        <IonGrid>
+                            <IonRow>
+                                <IonCol size = "2">te</IonCol>
+                                <IonCol>
+                                    <IonRow>
+                                        <IonText>You have donated:</IonText>
+                                    </IonRow>
+                                    <IonRow>
+                                        <div className = "donationValue">$0.00</div>
+                                    </IonRow>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonCardContent>
+                </IonCard>
+                <RecommendedSearchbar
+                    text = {text}
+                    setText = {setText}
+                    handleKeyDown = {handleKeyDown}/>   
+            </div>
+            <IonList>
+                {
+                   charities && charities.map((charity: any) => {
+                        return (
+                            <CharityItem
+                                key = {charity.business_number}
+                                data = {charity}/>
+                        )
+                    })
+                }
+            </IonList>
+
+        </div>
+    );
+};
+
+export default RecommendedCharityScreen;
