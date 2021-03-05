22b22e83be94eded0f79b36c69ae3dcb7f6f5aac
Joel Skrepnek <joel@koho.ca>
2021-03-04 19:54:16 -0800

    Look a like KOHO tabs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,12 @@ import {
   setupConfig,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle, heartCircleOutline } from 'ionicons/icons';
+import {
+  heartCircleOutline,
+  cardOutline,
+  cashOutline,
+  hammerOutline,
+} from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -40,55 +45,55 @@ import CharityInformation from './pages/CharityInformation';
 import CharityDonation from './pages/CharityDonation';
 
 setupConfig({
-  mode: 'ios'
-})
+  mode: 'ios',
+});
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
         <Route path="/information">
-            {/* This probably requires specific id */}
-            <CharityInformation />
-          </Route>
-          <Route path="/donation">
-            <CharityDonation />
-          </Route>
+          {/* This probably requires specific id */}
+          <CharityInformation />
+        </Route>
+        <Route path="/donation">
+          <CharityDonation />
+        </Route>
         <Route path="/tabs">
           <IonTabs>
             <IonRouterOutlet>
-              <Route exact path="/tabs/tab1">
+              <Route exact path="/tabs/spending">
                 <Tab1 />
               </Route>
-              <Route exact path="/tabs/tab2">
+              <Route exact path="/tabs/savings">
                 <Tab2 />
               </Route>
-              <Route path="/tabs/tab3">
+              <Route path="/tabs/tools">
                 <Tab3 />
               </Route>
               <Route path="/tabs/giving/landing">
                 <GivingLanding />
               </Route>
               <Route exact path="/tabs/">
-                <Redirect to="/tabs/tab1" />
+                <Redirect to="/tabs/spending" />
               </Route>
             </IonRouterOutlet>
             <IonTabBar slot="bottom">
-              <IonTabButton tab="tab1" href="/tabs/tab1">
-                <IonIcon icon={triangle} />
-                <IonLabel>Tab 1</IonLabel>
+              <IonTabButton tab="spending" href="/tabs/spending">
+                <IonIcon icon={cardOutline} />
+                <IonLabel>SPENDING</IonLabel>
               </IonTabButton>
-              <IonTabButton tab="tab2" href="/tabs/tab2">
-                <IonIcon icon={ellipse} />
-                <IonLabel>Tab 2</IonLabel>
+              <IonTabButton tab="savings" href="/tabs/savings">
+                <IonIcon icon={cashOutline} />
+                <IonLabel>SAVINGS</IonLabel>
               </IonTabButton>
-              <IonTabButton tab="tab3" href="/tabs/tab3">
-                <IonIcon icon={square} />
-                <IonLabel>Tab 3</IonLabel>
+              <IonTabButton tab="tools" href="/tabs/tools">
+                <IonIcon icon={hammerOutline} />
+                <IonLabel>TOOLS</IonLabel>
               </IonTabButton>
               <IonTabButton tab="giving" href="/tabs/giving/landing">
                 <IonIcon icon={heartCircleOutline} />
-                <IonLabel>Giving</IonLabel>
+                <IonLabel>GIVING</IonLabel>
               </IonTabButton>
             </IonTabBar>
           </IonTabs>
