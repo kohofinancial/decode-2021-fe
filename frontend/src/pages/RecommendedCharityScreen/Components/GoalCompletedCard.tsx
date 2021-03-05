62347d40de1a763039abea32e1b41c179d8dab5a
James Lee <jklee4365@gmail.com>
2021-03-05 17:20:28 -0500

    delte campaigns and route to thankyou page

--- frontend/src/pages/RecommendedCharityScreen/Components/GoalCompletedCard.tsx
+++ frontend/src/pages/RecommendedCharityScreen/Components/GoalCompletedCard.tsx
@@ -1,12 +1,24 @@
 import { IonCard, IonCardContent } from '@ionic/react';
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 //css
 import './GoalCompletedCard.css';
   
   const GoalCompletedCard: React.FC<any> = ({data}) => {
-      console.log(data);
+    const history = useHistory();
+
+    const onClickDonateNow = () => {
+        fetch("https://decode-be-2021.herokuapp.com/campaigns/" + data._id,{
+            method: 'DELETE',
+        }).then(res => {
+            return (res.ok) ?  res.json() : new Error("Mistake!")
+        }).then(data => {
+            history.push("/thankyou")
+        })
+    }
     return (
+        <div>
         <IonCard class = "goalCard">
             <IonCardContent className = "goalCompletedCard">
                 <div className = "goalCompletedContainer">
@@ -15,13 +27,14 @@ import './GoalCompletedCard.css';
                         <div style = {{fontWeight: "bold", fontSize: 23, marginLeft: 15}}>Goal completed!</div>
                     </div>
                     <div className = "filledBar"></div>
-                    <div className = "donateNowBtn">
+                    <div className = "donateNowBtn" onClick = {onClickDonateNow}>
                         <b>Donate now</b>
                     </div>
                     <div className = "seeMoreBtn">See more</div>
                 </div>
             </IonCardContent>
         </IonCard>
+        </div>
     );
   };
   
